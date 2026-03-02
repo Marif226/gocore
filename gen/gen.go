c3d81a52874bc4da1ab9427baac4eb6a00fcb589
Gabdylgaziz Zhagypar <g.zhagypar@redmadrobot.kz>
2026-03-02 15:47:22 +0500

    chore(go.mod): add indirect dependency on github.com/google/uuid v1.6.0

---
+++ gen/gen.go
@@ -0,0 +1,3 @@
+// Package gen provides generators and helpers for producing identifiers
+// and other runtime values (e.g. UUIDs).
+package gen
---
+++ gen/uuid.go
@@ -0,0 +1,29 @@
+package gen
+
+import (
+	"github.com/google/uuid"
+)
+
+// UUIDGenerator produces random unique identifiers (UUIDs).
+type UUIDGenerator func() uuid.UUID
+
+// UUID returns a UUID generator.
+func UUID() UUIDGenerator {
+	return RealUUIDGenerator()
+}
+
+// Next returns the next unique identifier.
+func (g UUIDGenerator) Next() uuid.UUID {
+	if g == nil {
+		return uuid.Nil
+	}
+
+	return g()
+}
+
+// RealUUIDGenerator returns a generator that produces new UUIDs via uuid.New.
+func RealUUIDGenerator() UUIDGenerator {
+	return func() uuid.UUID {
+		return uuid.New()
+	}
+}
